1d8bab1226638c7b7762794b01a6e8b07bf1d617
Sunghyun Park <madenmud@gmail.com>
2026-02-18 18:26:47 +0900

    chore: sync build info

--- public/js/version.js
+++ public/js/version.js
@@ -1,2 +1,2 @@
 /** Generated at build time. Do not edit. */
-window.__BUILD__ = { version: "v2.1.4", buildTime: "2026. 2. 18. 16시 52분 52초" };
+window.__BUILD__ = { version: "v2.1.4", buildTime: "2026. 2. 18. 18시 26분 47초" };
